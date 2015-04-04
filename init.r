05728adaeb5a02b79d3d0942e487dd6b79d0faf1
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2015-04-04 13:13:55 +0200

    Add data.table dependancy in init.r

--- init.r
+++ init.r
@@ -6,7 +6,7 @@
 ###########################################################
 # Update this line with the R packages to install:
 
-my_packages = c("randomForest")
+my_packages = c("randomForest", "data.table")
 
 ###########################################################
 
