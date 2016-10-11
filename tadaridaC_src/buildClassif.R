2388ad99c5306414aed3cb2f0bedd7433c22e487
YvesBas <yves.bas@gmail.com>
2016-10-11 15:26:20 +0200

    update output files

--- tadaridaC_src/buildClassif.R
+++ tadaridaC_src/buildClassif.R
@@ -3,7 +3,7 @@
 RSDB="C:/Users/yves/Documents/Tadarida/Tadarida-C/RSDB_sample"
 MRF="C:/Users/yves/Documents/Tadarida/Tadarida-C/tadaridaC_src/Modified_randomForest.R"
 #optional:
-SpeciesList=read.csv("C:/Users/yves/Documents/Tadarida-C/SpeciesList.csv") #to uncomment if a species grouping and/or filtering is necessary
+SpeciesList=read.csv("C:/Users/yves/Documents/Tadarida/Tadarida-C/SpeciesList.csv") #to uncomment if a species grouping and/or filtering is necessary
 ### A TESTER SANS FILTRE##
 GeoFilter="France" #to uncomment and edit if a species filtering is necessary
 
@@ -132,40 +132,46 @@ Sys.time()
 for (i in 1:50)
 {
   
-#randomly selecting 63% of sites to build the small forest
-SelSiteTemp=cbind(Site=levels(tabase3$Site)
-                  ,Sel=sample(0:1,nlevels(tabase3$Site),
-                       replace=T,prob=c(0.37,0.63)))
-tabase4=merge(tabase3,SelSiteTemp,by="Site")
-
-#designing sampling strata as a combination of species and site
-StrataTemp=as.factor(paste(as.character(tabase4$Nesp)
-                           ,as.character(tabase4$Sel)))
-
-#maximum sampled sound events per species
-SampMax=SubSamp*exp(i*(GradientSamp))*NbMoyCri
-#Note that this variable depend on i and thus will vary according to each small random forest
-#This is intended to build a large forest mixing a gradient of trees, from:
-#- trees using a maximum number of sound events for high performance on common species (beginning of the loop)
-#- trees using more and more balanced sound events per species to decrease bias towards common species (end of the loop)
-
-#Defining sampling strata according to both constraints (selected site and maximum number of sound events per species) 
-SampTemp=(as.numeric(table(StrataTemp))
-          *as.numeric(sapply(levels(StrataTemp)
-  ,FUN=function(x) strsplit(as.character(x),split=" ")[[1]][2])))
-SampTemp2=sapply(SampTemp,FUN=function(x) min(x,SampMax))
-
-gc()
-
-# building the "10 trees" random forest
-ClassifEspTemp=randomForest(as.formula(paste("Nesp ~ ",FormulCrit))
-                            ,data=tabase4,replace=F
-                            ,strata=StrataTemp
-                            ,sampsize=SampTemp2
-                            ,importance=F,ntree=10) 
-
-#combine it with previously build small forests
-if (exists("ClassifEspA")==TRUE) {ClassifEspA=combine(ClassifEspA,ClassifEspTemp)} else {ClassifEspA=ClassifEspTemp}
+  print(paste("forest n°",i,Sys.time()))
+  
+  #randomly selecting 63% of sites to build the small forest
+  Sel=vector()
+  while(sum(Sel)==0)
+  {Sel=sample(0:1,nlevels(tabase3$Site),
+              replace=T,prob=c(0.37,0.63))}
+  
+  SelSiteTemp=cbind(Site=levels(tabase3$Site),Sel)
+  
+  tabase4=merge(tabase3,SelSiteTemp,by="Site")
+  
+  #designing sampling strata as a combination of species and site
+  StrataTemp=as.factor(paste(as.character(tabase4$Nesp)
+                             ,as.character(tabase4$Sel)))
+  
+  #maximum sampled sound events per species
+  SampMax=SubSamp*exp(i*(GradientSamp))*NbMoyCri
+  #Note that this variable depend on i and thus will vary according to each small random forest
+  #This is intended to build a large forest mixing a gradient of trees, from:
+  #- trees using a maximum number of sound events for high performance on common species (beginning of the loop)
+  #- trees using more and more balanced sound events per species to decrease bias towards common species (end of the loop)
+  
+  #Defining sampling strata according to both constraints (selected site and maximum number of sound events per species) 
+  SampTemp=(as.numeric(table(StrataTemp))
+            *as.numeric(sapply(levels(StrataTemp)
+                               ,FUN=function(x) strsplit(as.character(x),split=" ")[[1]][2])))
+  SampTemp2=sapply(SampTemp,FUN=function(x) min(x,SampMax))
+  
+  gc()
+  
+  # building the "10 trees" random forest
+  ClassifEspTemp=randomForest(as.formula(paste("Nesp ~ ",FormulCrit))
+                              ,data=tabase4,replace=F
+                              ,strata=StrataTemp
+                              ,sampsize=SampTemp2
+                              ,importance=F,ntree=10) 
+  
+  #combine it with previously build small forests
+  if (exists("ClassifEspA")==TRUE) {ClassifEspA=combine(ClassifEspA,ClassifEspTemp)} else {ClassifEspA=ClassifEspTemp}
 }
 Sys.time()
 
