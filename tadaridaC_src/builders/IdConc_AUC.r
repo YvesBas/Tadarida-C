585c537b5976ab08105ae94f3a9ed3603847ad2b
YvesBas <yves.bas@gmail.com>
2019-04-01 07:35:16 +0200

    add ROC graph export

--- tadaridaC_src/builders/IdConc_AUC.r
+++ tadaridaC_src/builders/IdConc_AUC.r
@@ -14,7 +14,7 @@ ListSp=levels(as.factor(IdConc$IdMan))
 ROClist=list()
 AUCtot=vector()
 
-for (i in 2:length(ListSp))
+for (i in 1:length(ListSp))
 {
   Label=(IdConc$IdMan==ListSp[i])
   testSp=match(ListSp[i],names(IdConc))
@@ -22,7 +22,16 @@ for (i in 2:length(ListSp))
   #boxplot(as.data.frame(ScoreSp)[,1]~as.factor(Label))
   ROCSp=roc(Label,as.data.frame(ScoreSp)[,1])
   ROClist=c(ROClist,ROCSp)
-  plot(ROCSp$sensitivities,ROCSp$specificities,type="l",main=ListSp[i])
+  png(filename=paste0("C:/Users/Yves Bas/Documents/VigieChiro/AUC/",ListSp[i],"_"
+      ,FIdConc,".png"),res=100)
+  
+print(    plot(ROCSp,type="l",main=ListSp[i]
+       ,grid=c(0.1, 0.1),grid.col=c("green", "red"),xlim=c(0,1),ylim=c(0,1)
+       ,add=F)
+)
+dev.off()
+  plot(ROCSp$sensitivities,ROCSp$specificities,type="l",main=ListSp[i]
+       ,grid=c(0.1, 0.2))
   AUCSp=auc(ROCSp)
   AUCtot=c(AUCtot,AUCSp)
   print(paste(ListSp[i],AUCSp))
