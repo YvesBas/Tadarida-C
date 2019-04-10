06d49ebb34fb5021ed4ddfc57565529be8e8c7a9
YvesBas <yves.bas@gmail.com>
2019-04-10 21:54:52 +0200

    optimize memory consumption by moving AggContacts into block processing

--- tadaridaC_src/Ta_Tc.R
+++ tadaridaC_src/Ta_Tc.R
@@ -10,9 +10,9 @@ AggNbSp="AggNbSp.r"
 
 #get arguments from the command line
 args <- commandArgs(trailingOnly = TRUE)
-#uncomment the following lines if you prefer to do not use R in command line
+#uncomment the following lines if you pre mfer to do not use R in command line
 #args="C:/wamp64/www/wav/txt" #directory containing .ta files
-#args="E:/PI_CDL1804/5ae195607dcf87000f45bbfb/txt"
+#args[1]="D:/PI_20/DataPR_Net1&2/txt"
 #args[2]="ClassifEsp_LF_180320.learner"
 #args[2]="ClassifEsp_LF_180129.learner"
 #args[2]="ClassifEspFrance180303.learner"
@@ -23,7 +23,7 @@ args <- commandArgs(trailingOnly = TRUE)
 #args[4]=8 #HPF
 #args[5]=F #Reduc - obsolete
 #args[6]=F #TC
-#args[7]=500 #block size
+#args[7]=200 #block size
 #args[10]="SpeciesList.csv" #species list
 #args[11]="CNS_tabase3HF_France_IdConc.learner" #name of the species number" classifier
 #args[12]=T #if species number should be filtered or not
@@ -38,17 +38,16 @@ block=as.numeric(args[7])
 
 
 for (r in 1:ceiling(length(talistot)/block))
-{
+  {
 args[8]=block*(r-1)+1 #start number
 args[9]=block*r #end number
 source(ClassifC1)
 print(paste(r,ceiling(length(talistot)/block),Sys.time()))
+source(AggContacts)
 }
 
 print(gc())
 
-source(AggContacts)
-print(gc())
-
 source(AggNbSp)
 print(gc())
+
